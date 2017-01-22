0c1a6736eac381984bceb18afb925a3042cb947a
Adriano Ferrari Cardoso <adriano_cardoso93@hotmail.com>
2017-01-21 23:49:33 -0200

    Chaitin-Briggs prototype

---
+++ Chaitin-Briggs/CMakeFiles/LiveDebugVariables.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/LiveDebugVariables.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chaitin-Briggs/RegAllocCB-Mats/CMakeFiles/LLVMRegAllocCB.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LLVMRegAllocCB.dir/RegAllocCB.cpp.o"
+  "LLVMRegAllocCB.pdb"
+  "LLVMRegAllocCB.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LLVMRegAllocCB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
