f1254f83fb22cf44e754b9b4a1ff9165cc2adec1
Adriano Ferrari Cardoso <adriano_cardoso93@hotmail.com>
2017-01-22 00:30:47 -0200

    removing cmake trash

--- Chaitin-Briggs/CMakeFiles/LiveDebugVariables.cpp.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for LiveDebugVariables.cpp.
-# This may be replaced when dependencies are built.
--- Chaitin-Briggs/CMakeFiles/install-LiveDebugVariables.cpp.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-LiveDebugVariables.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/install-LiveDebugVariables.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
